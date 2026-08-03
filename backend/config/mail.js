da280d6fb3df643e0fe77e62d4c59e230a704ce7
Anas <anas.saifi4201@gmail.com>
2026-08-03 13:14:38 +0530

    remove the email bugs

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,16 +1,17 @@
 import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 
-dotenv.config(); 
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // TLS
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 export default transporter;
